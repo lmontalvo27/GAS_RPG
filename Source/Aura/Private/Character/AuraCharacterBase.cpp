a295733b3ed2a9f0cfa03e786d3991adb969d4ef
lmontalvo27 <lmontalvo1998@gmail.com>
2024-01-27 12:56:53 -0800

    Added Player and Enemy classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
