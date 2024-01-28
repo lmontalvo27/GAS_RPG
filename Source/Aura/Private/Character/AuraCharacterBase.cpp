57aa5023dca087e82b92561bc0e36630ea5f9d97
lmontalvo27 <lmontalvo1998@gmail.com>
2024-01-27 20:40:38 -0800

    Added Player controller

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -9,6 +9,10 @@ AAuraCharacterBase::AAuraCharacterBase()
  	
 	PrimaryActorTick.bCanEverTick = false;
 
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 
